823979f4f7107a0cb1781fca53468b7a10b91fe2
Guilherme Niimoto <guilherme.niimoto@bemobi.com.br>
2022-08-23 20:28:57 -0300

    Alterações da Seção 7 - WebUI

--- CleanArchMvc.Application/DTOs/ProductDTO.cs
+++ CleanArchMvc.Application/DTOs/ProductDTO.cs
@@ -1,12 +1,8 @@
 ﻿using CleanArchMvc.Domain.Entities;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CleanArchMvc.Application.DTOs
 {
@@ -17,32 +13,34 @@ namespace CleanArchMvc.Application.DTOs
         [Required(ErrorMessage = "The Name is Required")]
         [MinLength(3)]
         [MaxLength(100)]
-        public string Name { get; private set; }
+        [DisplayName("Name")]
+        public string Name { get; set; }
 
         [Required(ErrorMessage = "The Description is Required")]
         [MinLength(5)]
         [MaxLength(200)]
-        public string Description { get; private set; }
+        [DisplayName("Description")]
+        public string Description { get; set; }
 
         [Required(ErrorMessage = "The Price is Required")]
         [Column(TypeName = "decimal(18,2)")]
         [DisplayFormat(DataFormatString = "{0:C2}")]
         [DataType(DataType.Currency)]
         [DisplayName("Price")]
-        public decimal Price { get; private set; }
+        public decimal Price { get; set; }
 
         [Required(ErrorMessage = "The Stock is Required")]
         [Range(1, 9999)]
         [DisplayName("Stock")]
-        public int Stock { get; private set; }
+        public int Stock { get; set; }
 
         [MaxLength(250)]
         [DisplayName("Product Image")]
-        public string Image { get; private set; }
+        public string Image { get; set; }
 
         public Category Category { get; set; }
 
         [DisplayName("Categories")]
-        public int CateogoryId { get; set; }
+        public int CategoryId { get; set; }
     }
 }
--- CleanArchMvc.WebUI/Controllers/CategoriesController.cs
+++ CleanArchMvc.WebUI/Controllers/CategoriesController.cs
@@ -1,5 +1,7 @@
-﻿using CleanArchMvc.Application.Interfaces;
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
 
 namespace CleanArchMvc.WebUI.Controllers
@@ -19,6 +21,87 @@ namespace CleanArchMvc.WebUI.Controllers
             var categories = await _categoryService.GetCategories();
 
             return View(categories);
+        }
+
+        [HttpGet()]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryDTO categoryDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                await _categoryService.Add(categoryDTO);
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(categoryDTO);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var categoryVM = await _categoryService.GetById(id);
+
+            if (categoryVM == null) return NotFound();
+
+            return View(categoryVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(CategoryDTO categoryDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _categoryService.Update(categoryDTO);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(categoryDTO);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var categoryDto = await _categoryService.GetById(id);
+
+            if (categoryDto == null) return NotFound();
+
+            return View(categoryDto);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _categoryService.Remove(id);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var categoryDto = await _categoryService.GetById(id);
+
+            if (categoryDto == null) return NotFound();
+
+            return View(categoryDto);
         }
     }
 }
